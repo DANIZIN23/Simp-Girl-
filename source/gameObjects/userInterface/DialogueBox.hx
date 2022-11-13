890dae691f502307c761c827581fad9453a97640
DANIZIN <98532797+DANIZIN23@users.noreply.github.com>
2022-11-13 14:08:31 -0300

    Update DialogueBox.hx

--- source/gameObjects/userInterface/DialogueBox.hx
+++ source/gameObjects/userInterface/DialogueBox.hx
@@ -103,7 +103,7 @@ class DialogueBox extends FlxSpriteGroup
 		var truePath = Paths.file(file);
 
 		// load the json file
-		if (Assets.exists(dialoguePath))
+		if (sys.FileSystem.exists(dialoguePath))
 			return dialoguePath;
 		else
 			return truePath;
@@ -234,7 +234,7 @@ class DialogueBox extends FlxSpriteGroup
 			var boxJson = Paths.file('images/dialogue/boxes/$curBox/$curBox.json');
 
 			// load the json and sprite
-			boxData = haxe.Json.parse(Assets.getText(boxJson));
+			boxData = haxe.Json.parse(sys.io.File.getContent(boxJson));
 			box.frames = Paths.getSparrowAtlas('dialogue/boxes/$curBox/$curBox');
 
 			// get the states sectioon
@@ -320,7 +320,7 @@ class DialogueBox extends FlxSpriteGroup
 				var portraitJson = Paths.file('images/dialogue/portraits/$curCharacter/$curCharacter.json');
 
 				// load the json file
-				if (Assets.exists(portraitJson))
+				if (sys.FileSystem.exists(portraitJson))
 				{
 					portraitData = haxe.Json.parse(sys.io.File.getContent(portraitJson));
 					portrait.frames = Paths.getSparrowAtlas('dialogue/portraits/$curCharacter/$curCharacter');
