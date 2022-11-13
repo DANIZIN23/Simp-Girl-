4cfcf3a8a0f2794c05489fdd2bf774bae499a6a5
DANIZIN <98532797+DANIZIN23@users.noreply.github.com>
2022-11-13 13:27:00 -0300

    Update CoolUtil.hx

--- source/meta/CoolUtil.hx
+++ source/meta/CoolUtil.hx
@@ -5,14 +5,14 @@ import meta.state.PlayState;
 
 using StringTools;
 
-#if !html5
+
 import sys.FileSystem;
-#end
+
 
 class CoolUtil
 {
 	// tymgus45
-	public static var difficultyArray:Array<String> = ['HARD', "NORMAL", "EASY"];
+	public static var difficultyArray:Array<String> = ['EASY', "NORMAL", "HARD"];
 	public static var difficultyLength = difficultyArray.length;
 
 	public static function difficultyFromNumber(number:Int):String
@@ -62,22 +62,21 @@ class CoolUtil
 
 	public static function returnAssetsLibrary(library:String, ?subDir:String = 'assets/images'):Array<String>
 	{
-		//
 		var libraryArray:Array<String> = [];
-		#if !html5
-		var unfilteredLibrary = FileSystem.readDirectory(SUtil.getPath() + '$subDir/$library');
 
-		for (folder in unfilteredLibrary)
+		for (folder in Assets.list().filter(files -> files.contains('$subDir/$library')))
 		{
-			if (!folder.contains('.'))
-				libraryArray.push(folder);
+			// simulating da FileSystem.readDirectory?
+			var daFolder:String = folder.replace('$subDir/$library', '');
+			daFolder = daFolder.replace(daFolder.substring(daFolder.indexOf('/'), daFolder.length), ''); // fancy
+			if (!daFolder.startsWith('.') && !libraryArray.contains(daFolder))
+				libraryArray.push(daFolder);
 		}
-		trace(libraryArray);
-		#end
 
 		return libraryArray;
 	}
 
+	
 	public static function getAnimsFromTxt(path:String):Array<Array<String>>
 	{
 		var fullText:String = Assets.getText(path);
